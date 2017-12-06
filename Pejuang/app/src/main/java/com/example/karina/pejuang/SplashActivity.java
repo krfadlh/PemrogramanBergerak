8c4c0660180926b669d541329d2ec789fecab735
krfadlh@outlook.com <krfadlh@outlook.com>
2017-12-07 00:21:40 +0700

    Layout

---
+++ Pejuang/app/src/main/java/com/example/karina/pejuang/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.example.karina.pejuang;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+/**
+ * Created by Karena on 3/12/2017.
+ */
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.background);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
